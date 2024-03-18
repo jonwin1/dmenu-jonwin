d583f4efc70c848c5d85348d2fe534a0cb55b8c6
jonwin1 <jonatan.wincent@proton.me>
2024-03-18 10:23:49 +0100

    fuzzymatch

--- config.def.h
+++ config.def.h
@@ -3,10 +3,13 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0x80;     /* Amount of opacity. 0xff is opaque             */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Fira Code:size=12"
 };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
