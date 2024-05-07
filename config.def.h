73129d670bb52c1c76739363a88e35e5c062f455
jonwin1 <jonatan.wincent@google.com>
2024-05-07 20:36:06 +0200

    opaque

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0x80;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
